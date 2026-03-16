1f1e191ddb5a5359cb23f7b1173e3c89ae34be81
Shivamprajapatii <shivamprajapati4794@gmail.com>
2026-03-16 16:31:21 +0530

    Database Connected Print statement

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,7 @@ import { PrismaClient } from "@prisma/client";
 import { Pool } from "pg";
 
 dotenv.config();
-
 const databaseUrl = process.env.DATABASE_URL;
-
 if (!databaseUrl) {
     throw new Error("DATABASE_URL is not set");
 }
